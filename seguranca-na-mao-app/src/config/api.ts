b50991e4a3ea8a8938a473a6a6c2db62270e309a
FelipeDaros <felipe-daros@hotmail.com>
2024-07-01 21:25:45 -0300

    Ajuste

--- seguranca-na-mao-app/src/config/api.ts
+++ seguranca-na-mao-app/src/config/api.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
-import { useNetInfo } from "@react-native-community/netinfo";
-
-const { type } = useNetInfo();
 
 export const api = axios.create({
-  baseURL: type !== "wifi" ? 'http://192.168.10.172:3005' : "http://192.168.10.172:3005"
+  baseURL: "http://192.168.10.172:3005"
 });
